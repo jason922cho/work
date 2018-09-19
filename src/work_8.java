d358b227c4321f7309b33b11fcdffd6ca29a145c
周楨祐 <107021010@live,asia,edu.tw>
2018-09-19 17:09:52 +0800

    Initial commit

---
+++ src/work_8.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+
+public class work_8 {
+    public static void main(String[] args) {
+        Scanner snc=new Scanner(System.in);
+        float C1=snc.nextFloat();
+        float F2=C1*9/5+32;
+        System.out.println("華氏溫度為"+'\n'+F2);
+    }
+}
